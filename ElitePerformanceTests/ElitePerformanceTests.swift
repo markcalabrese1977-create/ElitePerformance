9c3454e04e0e66636ca61dc06cdbdc4ac3a059c1
Mark Calabrese <markcalabrese1977@gmail.com>
2025-11-11 19:46:23 -0500

    Initial Commit

---
+++ ElitePerformanceTests/ElitePerformanceTests.swift
@@ -0,0 +1,16 @@
+//
+//  ElitePerformanceTests.swift
+//  ElitePerformanceTests
+//
+//  Created by Mark Calabrese on 11/11/25.
+//
+
+import Testing
+
+struct ElitePerformanceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
